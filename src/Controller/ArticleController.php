2c5bce9f681a57095240f3c37371ef34878d4654
Ogheneogaga Fidelis Agi <aogaga@gmail.com>
2018-05-26 19:04:56 -0500

    just before adding the profiler

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,10 +10,11 @@ namespace App\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class ArticleController
+class ArticleController extends AbstractController
 {
 
     /**
@@ -28,6 +29,14 @@ class ArticleController
      * @Route("/news/{slug}")
      */
     public function show($slug){
-        return new Response(sprintf('future page to show the article: %s ', $slug));
+       // return new Response(sprintf('future page to show the article: %s ', $slug));
+        $comments = [
+            'I ate a normal rock one. It did not taste like bacon !',
+            'wooh! i\'m going on an all-steroid diet',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com'
+        ];
+        return $this->render('article/show.html.twig',
+            ['title'=> ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments]);
     }
 }
